e3ea3de258a6f703596595686a2ad206844767f8
Baptiste <Baptiste@MacBook-Pro-de-Baptiste.local>
2015-10-27 19:00:26 +0100

    Debut Implementation classes  Script et Sequence

--- Code/src/Script/Script.java
+++ Code/src/Script/Script.java
@@ -1,11 +1,23 @@
 package Script;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Script implements IScript {
 	
-	private IContexte contexte;
-	private List<Script> scripts;
+	//Devra peut-etre etre un Singleton
+	private static IContexte contexte;
 	
-	
+	//Juste pour voir une idee de ce a quoi ca ressemble
+	public static void main(String[] args) {
+		Script dessiner = new Dessiner();
+		Script boucle = new Boucle();
+		List<Script> scripts = new ArrayList<Script>();
+		scripts.add(dessiner);
+		scripts.add(boucle);
+		Script S = new Sequence(scripts);
+		S.execute();
+		
+		
+	}
 }
--- Code/src/Script/Sequence.java
+++ Code/src/Script/Sequence.java
@@ -1,11 +1,22 @@
 package Script;
 
+import java.util.List;
+
 public class Sequence extends Script{
+	
+	private List<Script> scripts;
+	
+
+	public Sequence(List<Script> scripts) {
+		super();
+		this.scripts = scripts;
+	}
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
 		
 	}
+	
 
 }
