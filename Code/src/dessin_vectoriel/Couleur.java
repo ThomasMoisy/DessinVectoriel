3530f001052fe1509b39a944e3cab196d2505842
Baptiste <bapt.maillot@gmail.com>
2015-11-08 19:02:35 +0100

    Utilisation d'enum pour Couleur

--- Code/src/dessin_vectoriel/Couleur.java
+++ Code/src/dessin_vectoriel/Couleur.java
@@ -1,7 +1,10 @@
 package dessin_vectoriel;
 
-public class Couleur {
+public enum Couleur {
 	
+	BLEU, VERT, ROUGE, JAUNE, ROSE, ORANGE, NOIR, GRIS, BLANC ;
+	
+	/*
 	public static final Integer bleu = 1;
 	public static final Integer vert = 2;
 	public static final Integer rouge = 3;
@@ -11,19 +14,33 @@ public class Couleur {
 	public static final Integer noir = 7;
 	public static final Integer gris = 8;
 	public static final Integer blanc = 9;
+	*/
+	private String name;
 	
-	private Integer couleur;
 	
-	public Couleur(Integer couleur){
-		this.couleur = couleur;
-	}
-	
-	public Integer getCouleur() {
-		return couleur;
-	}
-	
-	public void setCouleur(Integer couleur) {
-		this.couleur = couleur;
+	public String getName() {
+		switch (this) {
+		case  BLEU:
+			return "bleu";
+		case VERT:
+			return "bleu";
+		case ROUGE:
+			return "bleu";
+		case JAUNE:
+			return "bleu";
+		case ROSE:
+			return "bleu";
+		case ORANGE:
+			return "bleu";
+		case NOIR:
+			return "bleu";
+		case GRIS:
+			return "bleu";
+		case BLANC:
+			return "bleu";
+		default:
+			return null;
+		}
 	}
 
 }
