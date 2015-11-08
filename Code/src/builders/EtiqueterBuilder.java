6045be40d71a357ab75f329ee6c709b16192d45c
Baptiste <bapt.maillot@gmail.com>
2015-11-08 20:02:38 +0100

    Creation classe EtiqueterBuilder

---
+++ Code/src/builders/EtiqueterBuilder.java
@@ -0,0 +1,31 @@
+package builders;
+
+import dessin_vectoriel.Etiquette;
+import script.ContexteForme;
+import script.Script;
+
+public class EtiqueterBuilder extends ScriptBuilder {
+	
+	private FormeBuilder forme;
+	
+	public EtiqueterBuilder() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public EtiqueterBuilder selectionne(String identifiant) throws Exception {
+		forme = ContexteForme.contexte.lookup(identifiant);
+		return this;
+	}
+	
+	public Etiquette etiquette(){
+		
+	}
+
+	@Override
+	public Script script() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
